e217da85f314cc71fa2b13cd76a7daa945999ec4
travelingaries <edwardjhchang@gmail.com>
2021-08-28 22:57:05 +0900

    add procfile, requirements.txt, runtime.txt

--- django_auth_example/settings.py
+++ django_auth_example/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-5qwofcq#h(cu@qg%y2hw@ujm(mgo2yrpc#^$%(5ejs1%$2tldr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'django-auth-example-app.herokuapp.com']
 
 
 # Application definition
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -108,11 +109,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(PROJECT_DIR, "../static"),
+# Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
