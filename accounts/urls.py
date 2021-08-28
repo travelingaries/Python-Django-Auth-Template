c391ca688fbaa12f2718bbfce8418210b9dec8ee
travelingaries <edwardjhchang@gmail.com>
2021-08-28 17:46:41 +0900

    create complete sign up process

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-
-from .views import SignUpView
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
 ]
--- django_auth_example/settings.py
+++ django_auth_example/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(PROJECT_DIR, "static"),
+STATICFILES_DIRS = os.path.join(PROJECT_DIR, "../static"),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
