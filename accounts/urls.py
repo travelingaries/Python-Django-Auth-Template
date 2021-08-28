dd663f569cb54e879dd244c1d0cb39c3cfa216f8
travelingaries <edwardjhchang@gmail.com>
2021-08-28 19:45:24 +0900

    create login process

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
-    path('login/', views.login, name="login"),
+    path('login/', views.custom_login, name="login"),
 ]
